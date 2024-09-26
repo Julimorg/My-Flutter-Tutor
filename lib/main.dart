78d612ab3b4a09bb8192b991aa4b609443c494bd
Tran Kien Phong <kienphongtran2003@gmail.com>
2024-09-26 15:47:57 +0700

    Flutter

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+// 1. import thư viện
+import 'package:flutter/material.dart';
+
+// 2. Hàm Main
+void main() {
+  // 3. Khởi động app
+  runApp(const MaterialApp(
+    // Sử dụng thành phần xây dựng giao diện người dùng
+    home: Text("Hello World"),
+  )); // MaterialApp
+}
